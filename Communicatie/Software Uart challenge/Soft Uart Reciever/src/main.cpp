591d0960253b2365442fc848a7acc4a3ddc65043
Tiemon <i491758@fhict.nl>
2023-03-31 10:35:09 +0200

    Communicatie soft uart
    De transmitter is nu af en de data is afgelezen met de logic analyzer. Ik ga nu verder met de reciever helemaal af te maken.

--- Communicatie/Software Uart challenge/Soft Uart Reciever/src/main.cpp
+++ Communicatie/Software Uart challenge/Soft Uart Reciever/src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include "SoftwareSerial.h"
 
+#define RX_TESTPIN 0
+
 //SOFTSERIEL TUSSEN ARDUINO'S
-#define RX_PIN 0
+#define RX_PIN 3
 #define BAUD_RATE 9600
 #define BIT_DELAY_US 104 // = 1000000 / BAUD_RATE / 10 (10 bits per byte)
 
 String receivedData = "";
+int CurrentMicros;
 
 void ProcessData();
+
+void Micros_Bit_Delay(); 
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -19,34 +24,50 @@ void setup() {
 
 void loop() {
   
+  CurrentMicros = micros();
   if (digitalRead(RX_PIN) == LOW) 
   {
-    // start bit detected
-    Serial.println("Start bit recieved!");
-    digitalWrite(LED_BUILTIN, HIGH);
-    delayMicroseconds(BIT_DELAY_US);
+    // start bit gedetecteerd
+    //Serial.println("Start bit recieved!");
+    Micros_Bit_Delay();
     ProcessData();
-    Serial.println(receivedData);
+    //Serial.println(receivedData);
   }
-  
 }
 
 void ProcessData() 
 {
-    byte receivedByte = 0;
+    char receivedByte = 0;
 
-    //checking the data
+    //data controlleren
     for (int i = 0; i < 8; i++) {
+      CurrentMicros = micros();
       receivedByte |= (digitalRead(RX_PIN) << i);
-      delayMicroseconds(BIT_DELAY_US);
+      Micros_Bit_Delay();
     }
 
+    CurrentMicros = micros();
     if (digitalRead(RX_PIN) == HIGH) {
       // stop bit detected
-      Serial.println("Stopbit recieved!");
+      //Serial.println("Stopbit recieved!");
       //digitalWrite(LED_BUILTIN, LOW);
+      Serial.println(receivedByte);
 
       //recieved character is added to the array
-      receivedData += (char)receivedByte;
+      receivedData += receivedByte;
+      Micros_Bit_Delay();
     }
+    else 
+    {
+      Serial.println("Invalid data!");
+      return;
+    }
+}
+
+void Micros_Bit_Delay() 
+{
+  while (micros() < CurrentMicros + BIT_DELAY_US)
+  {
+    //Wachten totdat de 104 ms voorbij zijn
+  }
 }
